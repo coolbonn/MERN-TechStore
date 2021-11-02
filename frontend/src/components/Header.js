5ec08e0a8c2bf834b97540c859d5929a81762128
luka_C.B <lukaaslamazashvili20@gmail.com>
2021-11-02 17:37:29 +0400

    Some Changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,85 +1,95 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
-import AuthModal from './AuthModal'
-import UserDropdown from './UserDropdown'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import AuthModal from "./AuthModal";
+import UserDropdown from "./UserDropdown";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [openDropdown, setOpenDropdown] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [openDropdown, setOpenDropdown] = useState(false);
 
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    return () => (document.body.style.overflow = "unset");
+  }, [isOpen]);
 
   return (
     <div>
-      <header className='header'>
-        <NavLink to='/' className='logo' title='Tech Store | Home'>
-          <img src={'/images/tech-store-logo.png'} alt='Logo' width={80} />
+      <header className="header">
+        <NavLink to="/" className="logo" title="Tech Store | Home">
+          <img src={"/images/tech-store-logo.png"} alt="Logo" width={80} />
         </NavLink>
         <nav>
-          <div className='page-links'>
+          <div className="page-links">
             <NavLink
-              style={{ textDecoration: 'none' }}
-              activeClassName='active'
-              to='/cellphones'
+              style={{ textDecoration: "none" }}
+              activeClassName="active"
+              to="/cellphones"
             >
               <span>Cell Phones</span>
             </NavLink>
             <NavLink
-              style={{ textDecoration: 'none' }}
-              activeClassName='active'
-              to='/tvs'
+              style={{ textDecoration: "none" }}
+              activeClassName="active"
+              to="/tvs"
             >
               <span>TVs</span>
             </NavLink>
             <NavLink
-              style={{ textDecoration: 'none' }}
-              activeClassName='active'
-              to='/computers'
+              style={{ textDecoration: "none" }}
+              activeClassName="active"
+              to="/computers"
             >
               <span>Computers</span>
             </NavLink>
             <NavLink
-              style={{ textDecoration: 'none' }}
-              activeClassName='active'
-              to='/accessories'
+              style={{ textDecoration: "none" }}
+              activeClassName="active"
+              to="/accessories"
             >
               <span>Accessories</span>
             </NavLink>
           </div>
 
-          <div className='auth-user_btn'>
+          <div className="auth-user_btn">
             <NavLink
-              style={{ textDecoration: 'none' }}
-              activeClassName='active'
-              to='/cart'
+              style={{ textDecoration: "none" }}
+              activeClassName="active"
+              to="/cart"
             >
-              <div className='cart'>
+              <div className="cart">
                 <span>Cart</span>
-                <i className='fas fa-shopping-cart'></i>
+                <i className="fas fa-shopping-cart"></i>
               </div>
             </NavLink>
             {!userInfo ? (
-              <div className='auth-btn' onClick={() => setIsOpen(true)}>
-                <b className='signin'>Sign In</b>
-                <strong className='divider'>|</strong>
-                <b className='signup'>Sign Up</b>
+              <div className="auth-btn" onClick={() => setIsOpen(true)}>
+                <b className="signin">Sign In</b>
+                <strong className="divider">|</strong>
+                <b className="signup">Sign Up</b>
               </div>
             ) : (
               <>
-                <div className='user_dropdown_container'>
+                <div className="user_dropdown_container">
                   <div
-                    className='user_dropdown_btn'
+                    className="user_dropdown_btn"
                     onClick={() => setOpenDropdown(!openDropdown)}
                   >
                     {userInfo.isAdmin ? (
-                      <i className='fas fa-user-tie'></i>
+                      <i className="fas fa-user-tie"></i>
                     ) : (
-                      <i className='fas fa-user'></i>
+                      <i className="fas fa-user"></i>
                     )}
                     <h5>{userInfo.username}</h5>
-                    <i className='fas fa-caret-down'></i>
+                    <i className="fas fa-caret-down"></i>
                   </div>
                   <UserDropdown
                     openDropdown={openDropdown}
@@ -95,11 +105,11 @@ const Header = () => {
         isOpen={isOpen}
         onShut={() => setIsOpen(false)}
         modalClose={(e) =>
-          e.target.classList.contains('modal-wrapper') && setIsOpen(false)
+          e.target.classList.contains("modal-wrapper") && setIsOpen(false)
         }
       />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
